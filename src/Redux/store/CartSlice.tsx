da24b1bdc439bc02dd7a43d3ec4643a544b9cd1e
tushar <tush44644@gmail.com>
2023-12-29 10:01:46 +0530

    apply reducer method on Cart state to remove duplicates objects

--- src/Redux/store/CartSlice.tsx
+++ src/Redux/store/CartSlice.tsx
@@ -28,7 +28,11 @@ const cartSlice = createSlice({
   reducers: {
     // that function run operation when user click add to cart button
     addToCart: (state: any, action: { payload: any; type: any }): [] => {
-      state = [...state, action.payload];
+      let combineArray:{}[] = [...state, action.payload];
+      let resultOfFilter:{}[] = combineArray.reduce((acc:any, item:{}):any => {
+        return acc.includes(item) ? acc : [...acc,item];
+      },[]);
+      state = resultOfFilter;
       return state;
     },
     // that funtion run opertion to remove the item from cartSlice
